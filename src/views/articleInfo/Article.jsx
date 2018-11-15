be1d391e53be64fb22d700053590595f9789f3b5
siwen <623746556@qq.com>
2018-11-15 18:01:36 +0800

    优化文章详情页

--- src/views/articleInfo/Article.jsx
+++ src/views/articleInfo/Article.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Skeleton  } from 'antd';
 import './article.scss'
 import { get } from "../../static/js/http";
 
@@ -7,6 +8,7 @@ export default class Article extends Component {
     super(props)
     this.state = {
       id:null,
+      loadingArticle:true,
       articleInfo:{},
     }
   }
@@ -14,14 +16,16 @@ export default class Article extends Component {
     console.log(this.props.match);
     this.setState({id:this.props.match.params.id})
     get('/articleList').then(res=>{
-      this.setState({articleInfo:res[this.props.match.params.id]})
+      this.setState({articleInfo:res[this.props.match.params.id],loadingArticle:false})
     })
   }
   render() {
     return (
-      <div className="article_info">
-        <h2>[{this.state.articleInfo.tag}]&nbsp;{this.state.articleInfo.title}</h2>
-        <div className="text_info">{this.state.articleInfo.text}</div>
+      <div className="article_info_info">
+        <Skeleton loading={this.state.loadingArticle} active paragraph={{ rows: 6 }}  title={false}>
+          <h2>[{this.state.articleInfo.tag}]&nbsp;{this.state.articleInfo.title}</h2>
+          <div className="text_info">{this.state.articleInfo.text}</div>
+        </Skeleton>
       </div>
     )
   }
